a36c3308c261027a3db501f254c11e02c3c64128
mtshv <i.mityushev@gmail.com>
2025-01-31 21:18:41 +0200

    Enable React Navigation dev tools plugin for Expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,19 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {Stack, useNavigationContainerRef} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {useReactNavigationDevTools} from "@dev-plugins/react-navigation";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // Enable React Navigation dev tools plugin for Expo router:
+  const navigationRef = useNavigationContainerRef();
+  useReactNavigationDevTools(navigationRef);
+
   const colorScheme = useColorScheme();
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
